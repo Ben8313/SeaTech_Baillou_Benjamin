9fc3ea912e1030d0946285ef6fc8eb4108b2f02f
iutgeiitoulonE105 <touloniut@gmail.com>
2025-02-06 12:29:50 +0100

    update

--- C#/RobotInterface/MainWindow.xaml.cs
+++ C#/RobotInterface/MainWindow.xaml.cs
@@ -38,7 +38,7 @@ namespace RobotInterface
             timerAffichage.Start();
 
            
-            serialPort1 = new ExtendedSerialPort("COM3", 115200, Parity.None, 8, StopBits.One);
+            serialPort1 = new ExtendedSerialPort("COM8", 115200, Parity.None, 8, StopBits.One);
             serialPort1.DataReceived += SerialPort1_DataReceived;
             serialPort1.Open();
             InitializeComponent();
